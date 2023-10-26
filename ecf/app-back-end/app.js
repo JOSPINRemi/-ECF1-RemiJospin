7bcddb0cb5ce88ecf1c829b1b6f9cf5b2c851677
JOSPINRemi <remi.jospin@gmail.com>
2023-10-26 14:53:28 +0200

    ajout du package cors

--- ecf/app-back-end/app.js
+++ ecf/app-back-end/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import projects from "./routes/projects.js";
 import ProjectDao from "./dao/ProjectDao.js";
+import cors from "cors";
 
 const port = 3000;
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 export const projectDao = new ProjectDao();
 
 app.use(express.json());
+app.use(cors());
 app.use("/projects", projects);
 
 app.listen(port, () => {
